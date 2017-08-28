ec4d8bd6ca304c468e1d9a71358d968e984f5226
Dragan Eror <draganeror@gmail.com>
2017-08-28 12:00:05 +0200

    Added tmp files to files path.

--- _dev/gulp/settings/files.js
+++ _dev/gulp/settings/files.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   css: [pathSettings.dev.css + '/**/*.css'],
   js: [pathSettings.dev.js + '/**/*.js'],
-  svg: [pathSettings.dev.svg],
+  svg: [pathSettings.dev.svg + '/**/*.svg'],
+
   public: {
     css: [pathSettings.theme.css + '/**/*.css'],
     fonts: [
@@ -24,7 +25,25 @@ module.exports = {
     ],
     js: [pathSettings.theme.js + '/**/*.js'],
     svg: [pathSettings.theme.svg + '/**/*.svg']
+  },
+
+  tmp: {
+    css: [pathSettings.tmp.css + '/**/*.css'],
+    sass: [pathSettings.tmp.sass + '/**/*.sass'],
+    js: [pathSettings.tmp.js + '/**/*.js'],
+    images: [
+      pathSettings.tmp.images + '/**/*.png',
+      pathSettings.tmp.images + '/**/*.jpg'
+    ],
+    svg: [pathSettings.tmp.svg + '/**/*.svg'],
+    fonts: [
+      pathSettings.tmp.fonts + '/**/*.eot',
+      pathSettings.tmp.fonts + '/**/*.ttf',
+      pathSettings.tmp.fonts + '/**/*.woff',
+      pathSettings.tmp.fonts + '/**/*.woff2'
+    ]
   }
+
   // pattern_lab: [
   //   options.rootPath.pattern_lab + '**/*.json',
   //   options.rootPath.pattern_lab + '**/*.twig'
